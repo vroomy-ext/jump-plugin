9644e3105ccb67c6fc047512121d418a8fed80b7
Josh Montoya <josh@montoya.io>
2021-01-21 17:56:46 -0800

    implement CreateUserMW

--- users.go
+++ users.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/gdbu/jump/users"
 	"github.com/vroomy/common"
 )
@@ -22,36 +20,25 @@ type CreateUserResponse struct {
 // CreateUser is a handler for creating a new user
 func CreateUser(ctx common.Context) {
 	var (
-		req CreateUserRequest
-		err error
+		resp CreateUserResponse
+		err  error
 	)
 
-	if err = ctx.Bind(&req); err != nil {
+	if resp, err = createUser(ctx); err != nil {
 		ctx.WriteJSON(400, err)
-		return
-	}
-
-	fmt.Println("REQ?", req, err)
-
-	var resp CreateUserResponse
-	if resp.UserID, resp.APIKey, err = p.jump.CreateUser(req.Email, req.Password, "users"); err != nil {
-		ctx.WriteJSON(400, err)
-		return
-	}
-
-	// Set createdUserID field
-	ctx.Put("createdUserID", resp.UserID)
-
-	// Grab url values from request
-	q := ctx.Request().URL.Query()
-
-	// Check to see if redirect query value has been set
-	if redirect := q.Get("redirect"); len(redirect) > 0 {
-		ctx.Redirect(302, redirect)
 		return
 	}
 
 	ctx.WriteJSON(200, resp)
+}
+
+// CreateUserMW is a middleware for creating a new user
+func CreateUserMW(ctx common.Context) {
+	var err error
+	if _, err = createUser(ctx); err != nil {
+		ctx.WriteJSON(400, err)
+		return
+	}
 }
 
 // SignUp is a handler for a self sign-up for a new user
@@ -90,27 +77,15 @@ func SignUp(ctx common.Context) {
 func createUser(ctx common.Context) (resp CreateUserResponse, err error) {
 	var req CreateUserRequest
 	if err = ctx.Bind(&req); err != nil {
-		ctx.WriteJSON(400, err)
 		return
 	}
 
 	if resp.UserID, resp.APIKey, err = p.jump.CreateUser(req.Email, req.Password, "users"); err != nil {
-		ctx.WriteJSON(400, err)
 		return
 	}
 
 	// Set createdUserID field
 	ctx.Put("createdUserID", resp.UserID)
-
-	// Grab url values from request
-	q := ctx.Request().URL.Query()
-
-	// Check to see if redirect query value has been set
-	if redirect := q.Get("redirect"); len(redirect) > 0 {
-		ctx.Redirect(302, redirect)
-		return
-	}
-
 	return
 }
 
