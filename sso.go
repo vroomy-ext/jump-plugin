face7c267afe09bf7466b3b76992bea11018eaa3
Josh Montoya <josh@montoya.io>
2021-02-02 14:19:14 -0800

    account for users whom are already logged in with sso login flow

--- sso.go
+++ sso.go
@@ -7,8 +7,16 @@ import (
 // SSOLogin is the handler for logging in with SSO
 func SSOLogin(ctx common.Context) {
 	var err error
+	if ctx.Get("userID") != "" {
+		// User is already logged in, write no content success and return
+		ctx.WriteNoContent()
+		return
+	}
+
+	// Get login code from URL param
 	loginCode := ctx.Param("loginCode")
 
+	// Attempt to login with code
 	if err = p.jump.SSOLogin(ctx, loginCode); err != nil {
 		ctx.WriteJSON(400, err)
 		return
