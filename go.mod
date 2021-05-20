d9e73dc6746f4f78cb7034b0f8f13eb4102fd91d
Josh Montoya <josh@montoya.io>
2021-05-20 11:08:55 -0700

    implement SSOMultiLogin

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.15
 
 require (
 	github.com/gdbu/emailvalidator v0.5.0
-	github.com/gdbu/jump v0.12.11
+	github.com/gdbu/jump v0.12.12
 	github.com/gdbu/scribe v0.5.3
 	github.com/hatchify/errors v0.4.82
 	github.com/mojura/mojura v0.10.4
--- sso.go
+++ sso.go
@@ -24,3 +24,24 @@ func SSOLogin(ctx common.Context) {
 
 	ctx.WriteNoContent()
 }
+
+// SSOMultiLogin is the handler for logging in with SSO
+func SSOMultiLogin(ctx common.Context) {
+	var err error
+	if ctx.Get("userID") != "" {
+		// User is already logged in, write no content success and return
+		ctx.WriteNoContent()
+		return
+	}
+
+	// Get login code from URL param
+	loginCode := ctx.Param("loginCode")
+
+	// Attempt to login with code
+	if err = p.jump.SSOMultiLogin(ctx, loginCode); err != nil {
+		ctx.WriteJSON(400, err)
+		return
+	}
+
+	ctx.WriteNoContent()
+}
